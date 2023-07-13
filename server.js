8e8203d021dbe8dc7067a937ab5e4d5efcf829c7
Guganhub <63343444+Guganhub@users.noreply.github.com>
2023-07-13 14:22:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,14 @@ const app = express();
 
 app.use(express.json());
 
+app.get('/',(req,res)=>{
+    res.status(200).send(`
+    <h1>Student Mentor APi</h1>
+    
+    `)
+
+})
+
 
 app.post('/mentors', async (req, res) => {
     try {
